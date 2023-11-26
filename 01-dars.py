b8ce1adc068aa7c764da37ed696cea170a87a40f
KhasanovdilmurodPython <56468436+KhasanovdilmurodPython@users.noreply.github.com>
2023-11-26 14:29:39 +0500

    Update 01-dars.py

--- 01-dars.py
+++ 01-dars.py
@@ -11,20 +11,20 @@
 # familya = "Xasanov"
 # # print(f"mening ismim {ism}, familyam {familya}")
 
-# # # print("salom \n bu \tpython \tkursi")
-# # ism_sharif = f"{ism} {familya}"
-# # print(ism_sharif.upper())
-# # print(ism_sharif.lower())
-# # print(ism_sharif.title())
-# # # print(ism_sharif.capitalize())
-# # fruit = "   apricot    "
-# # print(fruit)
-# # print(f"men {fruit.lstrip()}ni yaxshi ko`raman")
-# # print(f"men {fruit.rstrip()}ni yaxxshi ko`raman")
-# # print(f"men {fruit.strip()}ni yaxshi ko`raman")
-# #print (" salom, men bugun \n python basic kursini boshladim")
-# # ism =input("Ismingiz nima?" + "\n>>>")
-# # print(f"foydalanuvchi ismi {ism.title()}")
+# print("salom \n bu \tpython \tkursi")
+# ism_sharif = f"{ism} {familya}"
+# print(ism_sharif.upper())
+# print(ism_sharif.lower())
+# print(ism_sharif.title())
+# # print(ism_sharif.capitalize())
+fruit = "   apricot    "
+print(fruit)
+print(f"men {fruit.lstrip()}ni yaxshi ko`raman")
+print(f"men {fruit.rstrip()}ni yaxxshi ko`raman")
+print(f"men {fruit.strip()}ni yaxshi ko`raman")
+print (" salom, men bugun \n python basic kursini boshladim")
+ism =input("Ismingiz nima?" + "\n>>>")
+print(f"foydalanuvchi ismi {ism.title()}")
 
 # kocha = input("Ko`cha \n>>>")
 # mahalla = input("Mahalla \n>>>")
