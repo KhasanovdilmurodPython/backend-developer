d6228454f91621a3c4e36f461312fc24f8422f74
KhasanovdilmurodPython <tatusf2015@gmail.com>
2023-11-28 16:34:17 +0500

    Update 06-dars.py

--- 06-dars.py
+++ 06-dars.py
@@ -25,18 +25,40 @@ Created on Tue Nov 28 10:34:34 2023
 #     print(phone)
 # fan = set(phones)
 # print(fan)
-python1 = {
-    "boolean":"mantiqiy tur",
-    "integer":"butun son",
-    "for":"sikl operatori",
-    "while":"sikl operatori",
-    "if":"shart operatori",
-    "opencv":"computer vision sohasi kutubxonasi"
+# python1 = {
+#     "boolean":"mantiqiy tur",
+#     "integer":"butun son",
+#     "for":"sikl operatori",
+#     "while":"sikl operatori",
+#     "if":"shart operatori",
+#     "opencv":"computer vision sohasi kutubxonasi"
     
+#     }
+# # for key,value in python1.items():
+# #     print(f"{key.capitalize()} - {value.capitalize()}")
+# print(python1[input("Qaysi operator izohini bilishni xohlaysiz? \n>>>")].capitalize())
+menu  = {
+    "manti": 25000,
+    "lag`mon": 22000,
+    "lavash": 30000,
+    "gulxonim": 20000,
+    "somsa": 5000,
+    "chuchvara": 25000,
+    "jiz": 35000,
+    "sho`rva": 18000
     }
-# for key,value in python1.items():
-#     print(f"{key.capitalize()} - {value.capitalize()}")
-print(python1[input("Qaysi operator izohini bilishni xohlaysiz? \n>>>")].capitalize())
+list1 =[]
+print("3 ta taom buyurtma bering:")
+for x in range(3):
+    list1.append(input(f"{x+1} - taom   \n>>>"))
+    
+    
+for y in list1:
+    if y in menu:
+        print(f"{y} -  {menu[y]} so`m")
+    else:
+        print(f"Kechirasiz, bizda {y} yo`q ")
+    
     
     
     
