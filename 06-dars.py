729662290a1e5047ed2b659bc3ea83517308aeb8
KhasanovdilmurodPython <tatusf2015@gmail.com>
2023-11-28 15:34:36 +0500

    Create 06-dars.py

---
+++ 06-dars.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 28 10:34:34 2023
+
+@author: User
+"""
+# phones = {
+#     "ali": "galaxy s21",
+#     "dilmurod": "iphone 12",
+#     "halim": "huawei",
+#     "ravshan": "galaxy a02"
+#     }
+# print(phones.items())
+# print(phones.keys())
+# print(phones.values())
+# for key, value in phones.items():
+#     print(f"Key: {key.title()}")
+#     print(f"Value: {value.title()}" + "\n")
+# my_phones  = ["iphone 12","nokia 2210","huawei"]
+# for phone in phones.values():
+#     if phone in my_phones:
+#         print(f"Listdagi telefonlar ichida \
+#               menda borlari: {phone.title()}")
+# for phone in sorted(phones):
+#     print(phone)
+# fan = set(phones)
+# print(fan)
+python1 = {
+    "boolean":"mantiqiy tur",
+    "integer":"butun son",
+    "for":"sikl operatori",
+    "while":"sikl operatori",
+    "if":"shart operatori",
+    "opencv":"computer vision sohasi kutubxonasi"
+    
+    }
+# for key,value in python1.items():
+#     print(f"{key.capitalize()} - {value.capitalize()}")
+print(python1[input("Qaysi operator izohini bilishni xohlaysiz? \n>>>")].capitalize())
+    
+    
+    
